d69c00647f4cd9daa2621bc1d9edbbed9e98aaea
ASUS <pavanshetty05@gmail.com>
2023-04-30 12:26:38 +0530

    Commit files first time

--- src/main/java/com/example/online/movi/onlineMovi/OnlineMoviApplication.java
+++ src/main/java/com/example/online/movi/onlineMovi/OnlineMoviApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OnlineMoviApplication {
 	
-
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineMoviApplication.class, args);
 	}
